09e8f1d4be65050b3fdf045ef05938793fcdc8e1
xuhui <1498472791@qq.com>
2017-10-20 14:31:00 +0800

    增加了 参数解析

--- Elasticsearch/conn.py
+++ Elasticsearch/conn.py
@@ -3,8 +3,13 @@
 from elasticsearch import exceptions
 from elasticsearch import Urllib3HttpConnection
 # Urllib3HttpConnection.timeout = 3
+import argparse
+parser = argparse.ArgumentParser(description="Request Elasticsearch API")
+parser.add_argument('-p','--port',type=int,required=True,help='specify a web port')
+args = parser.parse_args()
+port = args.port
 try:
-  respon_result = Urllib3HttpConnection(host='192.168.206.82').perform_request(method='GET',url='/',timeout=3)
+  respon_result = Urllib3HttpConnection(host='192.168.206.82',port=port).perform_request(method='GET',url='/',timeout=3)
   respon_code = respon_result[0]
 except exceptions.ConnectionError:
   result = 'es not response'
