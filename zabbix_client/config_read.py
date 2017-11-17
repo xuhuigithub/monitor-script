fb43235eb7c9851d5352dad8741cc7fe08ffc008
xuhui <1498472791@qq.com>
2017-11-17 10:37:37 +0800

    No change

--- zabbix_client/config_read.py
+++ zabbix_client/config_read.py
@@ -11,3 +11,5 @@ if __name__ == "__main__":
   username = yml_data['userinfo']['username']
   password = yml_data['userinfo']['password']
   print username, password
+  tmp = list()
+  tmp.split(',')
--- zabbix_client/main.py
+++ zabbix_client/main.py
@@ -3,7 +3,47 @@
 from config_read import yconfig_read
 from Zabbix_api import Zabbix
 
-Server = Zabbix(uri='192.168.203.91',username='admin',password='zabbix_king')
-hosts = yconfig_read('./hostinfo.yml')
-result = Server.createhosts(hosts=hosts)
-print result
+if __name__ == "__main__":
+  Server = Zabbix(uri='192.168.203.91',username='admin',password='zabbix_king')
+  hosts = yconfig_read('./hostinfo.yml')
+  result = Server.createhosts(hosts=hosts)
+  for i in result:
+    print i
+  # media_data = {
+  #   "jsonrpc": "2.0",
+  #   "method": "user.create",
+  #   "params": {
+  #       "alias": "宫新胜",
+  #       "passwd": "Chinadaas@2017",
+  #       "usrgrps": [
+  #         {
+  #           "usrgrpid": "26",
+  #         }
+  #       ],
+  #       "user_medias":[
+  #         {
+  #           "mediatypeid" : "5",
+  #           "sendto": "gongxinsheng@chinadaas.com",
+  #           "active": 0,
+  #           "severity": 63,
+  #           "period": "1-7,00:00-24:00",
+  #         },
+  #         {
+  #           "mediatypeid": "6",
+  #           "sendto": "18610811144",
+  #           "active": 0,
+  #           "severity": 63,
+  #           "period": "1-7,00:00-24:00",
+  #         }
+  #       ],
+  #   },
+  # }
+  # data = {
+  #   "jsonrpc": "2.0",
+  #   "method": "mediatype.get",
+  #   "params": {
+  #     "output": "extend"
+  #   },
+  # }
+  # result = Server.commongateway(media_data)
+  # print result
